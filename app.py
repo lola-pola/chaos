ad66baf0326edf1fc7486557289c62d01f33473c
Elhay Efrat <elhayefrat@elhays.local>
2022-12-28 12:36:31 +0200

    adijng
    Signed-off-by: Elhay Efrat <elhayefrat@elhays.local>

--- app.py
+++ app.py
@@ -58,4 +58,4 @@ def receive_message(connection_string: str ,queue: str  , ):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=80)
